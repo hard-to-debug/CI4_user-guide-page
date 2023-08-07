d0b63879876b71ddace247bde596b41d7f81e352
hard-to-debug <matusiak.cezary@gmail.com>
2023-08-07 23:54:14 +0200

    Routing

---
+++ app/Views/pages/about.php
@@ -0,0 +1,3 @@
+<p class="about">
+    TEST TEST About
+</p>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,3 @@
+<p class="home__text">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam possimus quidem repellat ratione mollitia fugiat autem ut eligendi rerum dolores sit, neque aut soluta magnam qui cupiditate dolore. Quibusdam, iure?
+</p>
